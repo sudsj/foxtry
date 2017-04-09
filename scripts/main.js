d7c821b37ede7874aa7b8a3919d29dab6912e8f1
sudsj <sudhanshuj007@gmail.com>
2017-04-09 18:04:12 +0530

    json tried

--- scripts/main.js
+++ scripts/main.js
@@ -24,9 +24,36 @@ if(!localStorage.getItem('name')){
 	setUserName();
 } else {
 	var storedName = localStorage.getItem('name');
-	myHeading.textContent = 'Hi Super-Cool User, ' + storedName;
+	myHeading.textContent = 'Welcome Back! Super-Cool User, ' + storedName;
 }
 
 myButton.onclick = function(){
 	setUserName();
 }
+
+var myObj = { "name":"Sudhanshu", "age":18, "cars": null };
+for (x in myObj) {
+    document.getElementById("extra").innerHTML += x + " : "+ myObj[x] +"<br>";
+}
+
+var nestedObj = {"Hobbies":{"1":"Skating", "2":"Reading","3":"Programming"}}
+for (x in nestedObj.Hobbies) {
+	document.getElementById("hobbies").innerHTML += x + " : " + nestedObj.Hobbies[x] ;
+}
+myObj = {
+    "name":"John",
+    "age":30,
+    "cars": [
+        { "name":"Ford", "models":[ "Fiesta", "Focus", "Mustang" ] },
+        { "name":"BMW", "models":[ "320", "X3", "X5" ] },
+        { "name":"Fiat", "models":[ "500", "Panda" ] }
+    ]
+ }
+ for (i in myObj.cars) {
+    x += "<h3>" + myObj.cars[i].name + "</h3>";
+    for (j in myObj.cars[i].models) {
+        x +=" " + myObj.cars[i].models[j] + " ";
+    }
+}
+
+document.getElementById("cars").innerHTML += x;
